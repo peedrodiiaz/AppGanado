e8de34d7ee6266bc3080b7a0b8e36b3d913f30f1
Pedro Díaz Garduño <diaz.gaped24@triana.salesianos.edu>
2025-04-28 09:14:07 +0200

    Conf spring

---
+++ DiazGardunoPedro/src/main/java/com/salesianostriana/dam/DiazGardunoPedro/DiazGardunoPedroApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.DiazGardunoPedro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiazGardunoPedroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiazGardunoPedroApplication.class, args);
+	}
+
+}
---
+++ DiazGardunoPedro/src/test/java/com/salesianostriana/dam/DiazGardunoPedro/DiazGardunoPedroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.DiazGardunoPedro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiazGardunoPedroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
