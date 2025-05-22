5e648cbe24743ae8fa81b9eebba650b90adc64cf
Pedro Díaz Garduño <diaz.gaped24@triana.salesianos.edu>
2025-05-22 08:13:24 +0200

    FIN

--- DiazGardunoPedro/src/main/resources/import.sql
+++ DiazGardunoPedro/src/main/resources/import.sql
@@ -17,7 +17,6 @@ INSERT INTO raza (id, nombre) VALUES (14, 'Pirenaica');
 ALTER TABLE raza ALTER COLUMN id RESTART WITH 15;
 
 INSERT INTO vaca (id, peso, num_identificacion, fecha_nacimiento, fecha_parto, raza_id, vendida, precio_venta) VALUES (1, 650.4, 1001, '2020-05-15', '2023-11-10', 1, false, 0.0);
-INSERT INTO vaca (id, peso, num_identificacion, fecha_nacimiento, fecha_parto, raza_id, vendida, precio_venta) VALUES (1, 650.4, 1001, '2020-05-15', '2023-11-10', 1, false, 0.0);
 INSERT INTO vaca (id, peso, num_identificacion, fecha_nacimiento, fecha_parto, raza_id, vendida, precio_venta) VALUES (2, 700.0, 1002, '2019-03-20', '2022-09-15', 2, true, 1200.0);
 INSERT INTO vaca (id, peso, num_identificacion, fecha_nacimiento, fecha_parto, raza_id, vendida, precio_venta) VALUES (3, 680.5, 1003, '2021-01-10', '2023-12-01', 3, false, 0.0);
 INSERT INTO vaca (id, peso, num_identificacion, fecha_nacimiento, fecha_parto, raza_id, vendida, precio_venta) VALUES (4, 720.2, 1004, '2018-07-25', '2021-10-30', 4, true, 1350.0);
@@ -32,7 +31,6 @@ INSERT INTO vaca (id, peso, num_identificacion, fecha_nacimiento, fecha_parto, r
 INSERT INTO vaca (id, peso, num_identificacion, fecha_nacimiento, fecha_parto, raza_id, vendida, precio_venta) VALUES (13, 695.0, 1013, '2020-06-20', '2023-11-01', 13, false, 0.0);
 INSERT INTO vaca (id, peso, num_identificacion, fecha_nacimiento, fecha_parto, raza_id, vendida, precio_venta) VALUES (14, 740.0, 1014, '2019-02-15', '2022-10-05', 14, true, 1450.0);
 INSERT INTO vaca (id, peso, num_identificacion, fecha_nacimiento, fecha_parto, raza_id, vendida, precio_venta) VALUES (15, 685.0, 1015, '2021-05-10', '2023-12-15', 1, false, 0.0);	
-
 
 
 ALTER TABLE vaca ALTER COLUMN id RESTART WITH 16;
